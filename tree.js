aebc9e2a7c31dda215e6e4f8d65d66867a5482bc
guskirb <anguskirby1998@gmail.com>
2024-04-25 19:19:12 +0100

    Fix tree not assigning nodes bug

--- tree.js
+++ tree.js
@@ -8,10 +8,10 @@ class Tree {
         if (array.length === 0) {
             return null;
         }
-        let mid = Math.floor(array.length / 2);
+        let mid = parseInt(array.length / 2);
         let root = new Node(array[mid]);
 
-        root.left = this.buildTree(array.slice(0, mid - 1));
+        root.left = this.buildTree(array.slice(0, mid));
         root.right = this.buildTree(array.slice(mid + 1));
 
         return root;
