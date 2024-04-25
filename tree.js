9b91dafe56e402e1371b7d52b7609daa651868a5
guskirb <anguskirby1998@gmail.com>
2024-04-26 00:31:13 +0100

    Add delete value method

--- tree.js
+++ tree.js
@@ -2,12 +2,15 @@ import Node from "./node.js";
 import mergeSortFunc from "./merge.js";
 
 class Tree {
+    constructor(array) {
+        this.tree = this.buildTree(array);
+    }
 
     buildTree(array) {
-        array = this.filterSort(array);
         if (array.length === 0) {
             return null;
         }
+        array = this.filterSort(array);
         let mid = parseInt(array.length / 2);
         let root = new Node(array[mid]);
 
@@ -36,12 +39,81 @@ class Tree {
         )
         return array = mergeSortFunc(array);
     }
+
+    insert(value) {
+        let current = this.tree;
+
+        while (current.right || current.left) {
+            if (value < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        if (value < current.data) {
+            current.left = new Node(value);
+        } else {
+            current.right = new Node(value);
+        }
+    }
+
+    findValue(value) {
+        let current = this.tree;
+
+        while (current && current.data !== value) {
+            if (value < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        if (current === null) {
+            console.log("Enter a valid number")
+        }
+        return current;
+    }
+
+    deleteItem(node, value) {
+        if (node === null) {
+            return node;
+        }
+
+        if (value < node.data) {
+            node.left = this.deleteItem(node.left, value);
+        } else if (value > node.data) {
+            node.right = this.deleteItem(node.right, value);
+        } else {
+            if (node.left === null) {
+                return node.right;
+            } else if (node.right === null) {
+                return node.left;
+            }
+
+            node.data = this.minValue(node.right);
+            node.right = this.deleteItem(node.right, node.data)
+        }
+        return node;
+    }
+
+    minValue(node) {
+        let value = node.data;
+        while (node.left !== null) {
+            value = node.left.data;
+            node = node.left;
+        }
+        return value;
+    }
 }
 
-let newTree = new Tree;
-let myNode = newTree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-newTree.prettyPrint(myNode);
-console.log(myNode.left.right);
+let newTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+// let myNode = newTree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+newTree.insert(400);
+newTree.insert(500);
+newTree.insert(450);
+newTree.prettyPrint(newTree.tree);
+newTree.deleteItem(newTree.tree, 67);
+newTree.prettyPrint(newTree.tree);
+
 
 
 
